eb3a9b0dec561f32cbf03e55e186b524f1dbec60
localedtech <bridget@localed.co>
2018-11-18 17:09:41 -0500

    user story 2

---
+++ ChooseLevel.java
@@ -0,0 +1,34 @@
+package comp840project;
+
+import java.util.Scanner;
+
+public class ChooseLevel {
+
+	public static void main(String[] args)
+	{
+		//Player p = new Player();
+
+		Scanner scanner = new Scanner(System.in);
+		boolean signedIn = true;
+
+		while (signedIn= true) {
+			System.out.println("Choose a level: Easy, Medium, Hard");
+			String input = scanner.nextLine();
+			String[] words = input.split(" ");
+
+			switch(words[0]) {
+			case "Easy":
+				break;
+			case "Medium":
+				break;
+			case "Hard":
+				break;
+
+			default:
+				System.out.println("Command not recognised");
+
+			}
+		}
+		scanner.close();
+	}
+}
